c98404579c1db49ad0ff80863f68214c9a5ab3f4
Ana <104568665+Lazy020@users.noreply.github.com>
2023-06-29 02:04:20 +0000

    Fixe

---
+++ main.js
@@ -0,0 +1,25 @@
+function openImage(element) {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+  
+    modalImage.src = element.src;
+    modal.style.display = 'block';
+  }
+  
+  function closeImage() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+  }
+  function openImage(element) {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+  
+    modalImage.src = element.src;
+    modal.style.display = 'block';
+  }
+  
+  function closeImage() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+  }
+    
