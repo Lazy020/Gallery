9900a54453eea515f751a4b32db08f3dc528f40e
Ana <104568665+Lazy020@users.noreply.github.com>
2023-06-29 19:47:11 +0000

    AHHHHHH, i need help, mostly mental

--- main.js
+++ main.js
@@ -1,65 +1,68 @@
 let currentImageIndex = 0; // Índice da imagem atual
 
 function openImage(element) {
-  const modal = document.getElementById('modal')
-  const modalImage = document.getElementById('modal-image')
-  const totalImages = document.getElementById('total-images')
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modal-image');
+  const totalImages = document.getElementById('total-images');
 
-  const galleryImages = document.querySelectorAll('.single-galeria img')
-  const clickedImageIndex = Array.from(galleryImages).indexOf(element)
-  
-  currentImageIndex = clickedImageIndex
-  modalImage.src = element.src
-  modal.style.display = 'block'
-  
-  modal.addEventListener('click', showNextImage)
-  createModalNavButtons()
-  
-  totalImages.textContent = galleryImages.length
+  const galleryImages = document.querySelectorAll('.single-galeria img');
+  const clickedImageIndex = Array.from(galleryImages).indexOf(element);
+
+  currentImageIndex = clickedImageIndex;
+  modalImage.src = element.src;
+  modal.style.display = 'block';
+
+  createModalNavButtons();
+
+  totalImages.textContent = galleryImages.length;
 }
 
 function showNextImage() {
-  const modalImage = document.getElementById('modal-image')
-  const galleryImages = document.querySelectorAll('.single-galeria img')
-  const numImages = galleryImages.length
-  
-  currentImageIndex = (currentImageIndex + 1) % numImages
-  modalImage.src = galleryImages[currentImageIndex].src
+  const modalImage = document.getElementById('modal-image');
+  const galleryImages = document.querySelectorAll('.single-galeria img');
+  const numImages = galleryImages.length;
+
+  currentImageIndex = (currentImageIndex + 1) % numImages;
+  modalImage.src = galleryImages[currentImageIndex].src;
 }
 
 function showPrevImage() {
-  const modalImage = document.getElementById('modal-image')
-  const galleryImages = document.querySelectorAll('.single-galeria img')
-  const numImages = galleryImages.length
-  
-  currentImageIndex = (currentImageIndex - 1 + numImages) % numImages
-  modalImage.src = galleryImages[currentImageIndex].src
+  const modalImage = document.getElementById('modal-image');
+  const galleryImages = document.querySelectorAll('.single-galeria img');
+  const numImages = galleryImages.length;
+
+  currentImageIndex = (currentImageIndex - 1 + numImages) % numImages;
+  modalImage.src = galleryImages[currentImageIndex].src;
 }
 
 function createModalNavButtons() {
-  const modal = document.getElementById('modal')
-  
-  const modalNav = document.createElement('div')
-  modalNav.classList.add('modal-nav')
+  const modal = document.getElementById('modal');
+
+  const modalNav = document.createElement('div');
+  modalNav.classList.add('modal-nav');
   modalNav.innerHTML = `
-    <span class="modal-nav-btn prev" onclick="showPrevImage()">&lt;</span>
-    <span class="modal-nav-btn next" onclick="showNextImage()">&gt;</span>
+    <span class="modal-nav-btn prev">&lt;</span>
+    <span class="modal-nav-btn next">&gt;</span>
   `;
-  modal.appendChild(modalNav)
+  modal.appendChild(modalNav);
+
+  const prevButton = modalNav.querySelector('.prev');
+  const nextButton = modalNav.querySelector('.next');
+  prevButton.addEventListener('click', showPrevImage);
+  nextButton.addEventListener('click', showNextImage);
 }
 
 function closeImage() {
-  const modal = document.getElementById('modal')
-  
-  modal.style.display = 'none'
-  modal.removeEventListener('click', showNextImage)
-  removeModalNavButtons()
+  const modal = document.getElementById('modal');
+
+  modal.style.display = 'none';
+  removeModalNavButtons();
 }
 
 function removeModalNavButtons() {
-  const modal = document.getElementById('modal')
-  const modalNav = document.querySelector('.modal-nav')
-  
+  const modal = document.getElementById('modal');
+  const modalNav = document.querySelector('.modal-nav');
+
   if (modalNav) {
     modal.removeChild(modalNav);
   }
